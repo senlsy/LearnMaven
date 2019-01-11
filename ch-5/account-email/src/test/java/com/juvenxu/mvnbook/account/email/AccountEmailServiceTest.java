ab0d1959f1d6abc90b16905031ec886f7bb768a9
mark.lin <marklin@qeeka.com>
2019-01-11 18:46:31 +0800

    【.】

--- ch-5/account-email/src/test/java/com/juvenxu/mvnbook/account/email/AccountEmailServiceTest.java
+++ ch-5/account-email/src/test/java/com/juvenxu/mvnbook/account/email/AccountEmailServiceTest.java
@@ -15,35 +15,42 @@ import com.icegreen.greenmail.util.GreenMailUtil;
 import com.icegreen.greenmail.util.ServerSetup;
 
 
-public class AccountEmailServiceTest
-{
-    
+public class AccountEmailServiceTest {
+
+    /*email.protocol=smtp
+    email.host=localhost
+    email.port=25
+    email.username=test@juvenxu.com
+    email.password=123456
+    email.auth=true
+    email.systemEmail=test@juvenxu.com*/
+
     private GreenMail greenMail;
-    
+
     @Before
     public void startMailServer() throws Exception {
-        greenMail=new GreenMail(ServerSetup.SMTP);
+        greenMail = new GreenMail(ServerSetup.SMTP);
         greenMail.setUser("test@juvenxu.com", "123456");
         greenMail.start();
     }
-    
+
     @Test
     public void testSendMail() throws Exception {
-        
-        ApplicationContext ctx=new ClassPathXmlApplicationContext("account-email.xml");
-        AccountEmailService accountEmailService=(AccountEmailService)ctx.getBean("accountEmailService");
 
-        String subject="Test Subject";
-        String htmlText="<h3>Test</h3>";
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("account-email.xml");
+        AccountEmailService accountEmailService = (AccountEmailService) ctx.getBean("accountEmailService");
+        String subject = "Test Subject";
+        String htmlText = "<h3>Test</h3>";
         accountEmailService.sendMail("test2@juvenxu.com", subject, htmlText);
         //
         greenMail.waitForIncomingEmail(2000, 1);
-        Message[] msgs=greenMail.getReceivedMessages();
+
+        Message[] msgs = greenMail.getReceivedMessages();
         assertEquals(1, msgs.length);
         assertEquals(subject, msgs[0].getSubject());
         assertEquals(htmlText, GreenMailUtil.getBody(msgs[0]).trim());
     }
-    
+
     @After
     public void stopMailServer() throws Exception {
         greenMail.stop();
